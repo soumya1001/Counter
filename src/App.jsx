df988ffa229ca0492870f0db6b9ae90f56b7c8f9
Soumyajit Bag <soumyajitbag2001@gmail.com>
2024-10-25 01:11:23 +0530

    title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,19 @@ function App() {
   return (
     <>
       <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center bg-[#42ce88]'>
-        <h1 className='text-lg font-bold'>Increment & Decrement</h1>
-        <div className='flex flex-row items-center bg-white py-2 m-2 rounded-sm'>
-          <button onClick={decreaseHandler} className='px-4 text-lg font-bold border-r-2 '>
+        <h1 className='text-3xl font-bold p-5'>Counter App</h1>
+        <div className='flex flex-row items-center bg-white py-2 mt-10 rounded-sm'>
+          <button onClick={decreaseHandler} className='px-7 text-lg font-bold border-r-2 '>
             -
           </button>
-          <div className='px-4 text-lg font-bold'>
+          <div className='px-7 text-lg font-bold'>
             {count}
           </div>
-          <button onClick={increaseHandler} className='px-4 text-lg font-bold border-l-2'>
+          <button onClick={increaseHandler} className='px-7 text-lg font-bold border-l-2'>
             +
           </button>
         </div>
-        <button onClick={resetHandler} className='bg-blue-500 text-white px-2 rounded-sm text-lg font-medium'>Reset</button>
+        <button onClick={resetHandler} className='m-5 bg-blue-500 text-white px-2 rounded-sm text-lg font-medium'>Reset</button>
       </div>
     </>
   )
